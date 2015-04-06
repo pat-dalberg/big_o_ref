410ea4da7d2a4328496b59ad3fde61ca61f72127
Pat Dalberg <pat.dalberg@gmail.com>
2015-04-06 16:46:26 -0500

    refactored to add examples and added d3 package for charting/visualization

--- big-o-ref/big-o-ref.js
+++ big-o-ref/big-o-ref.js
@@ -1,8 +1,4 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  //Session.setDefault('counter', 0);
-
-//  Meteor.startup(function(){
 /*
 notation schema
 name :
@@ -24,23 +20,57 @@ n * n factorial / O(n * n!)
 
 
   Notations = new Mongo.Collection("notations");
-  Notations.insert({name : "constant", formula : "O(1)", order : 0});
-  Notations.insert({name : "logarithmic", formula : "O(log n)", order : 1});
-  Notations.insert({name : "linear", formula : "O(n)", order : 2});
-  Notations.insert({name : "quadratic", formula : "O(n^2)", order : 3});
-  Notations.insert({name : "expotential", formula : "O(c^n), c > 1", order : 4});
-  Notations.insert({name : "factorial", formula : "O(n!)", order : 5});
-  Notations.insert({name : "n * n factorial", formula : "O(n * n!)", order : 6});
+  Notations.insert({name : "constant",
+   formula : "O(1)",
+   order : 0,
+   examples: "Array access, Stack insertion and deletion, Singly-Linked List insertion and deletion, Doubly-Linked List insertion and deletion, Hash Table search, insertion and deletion, Determining if a binary number is even or odd", 
+   wikiLink : "http://en.wikipedia.org/wiki/Time_complexity#Constant_time"});
+
+  Notations.insert({name : "logarithmic", 
+    formula : "O(log n)", 
+    order : 1, 
+    examples: "Binary Search Tree access, search, insertion and deletion, Red-Black Tree access, search, insertion and deletion, Finding an item in a sorted array with a binary search or a balanced search tree", 
+    wikiLink :"http://en.wikipedia.org/wiki/Time_complexity#Logarithmic_time"});
+  
+  Notations.insert({name : "linear", 
+    formula : "O(n)", 
+    order : 2, 
+    examples: "Array search and insertion, deletion, Stack access and search, Singly-Linked List access and search, Doubly-Linked List access and search, Best Bubble Sort, Best Insertion Sort, Best Shell Sort", 
+    wikiLink :"http://en.wikipedia.org/wiki/Time_complexity#Linear_time"});
+  
+  Notations.insert({name : "quadratic", 
+    formula : "O(n^2)", 
+    order : 3, 
+    examples: "Selection Sort, Multiplying two n-digit numbers by a simple algorithm", 
+    wikiLink :"http://en.wikipedia.org/wiki/Time_complexity#Sub-quadratic_time"});
+  
+  Notations.insert({name : "expotential", 
+    formula : "O(c^n), c > 1", 
+    order : 4, 
+    examples: "Determining if two logical statements are equivalent using brute-force search", 
+    wikiLink :"http://en.wikipedia.org/wiki/Time_complexity#Exponential_time"});
+  
+  Notations.insert({name : "factorial", 
+    formula : "O(n!)", 
+    order : 5, 
+    examples: "Solving the traveling salesman problem via brute-force search", 
+    wikiLink :"http://en.wikipedia.org/wiki/Bell_number"});
+  
+  Notations.insert({name : "n * n factorial", 
+    formula : "O(n * n!)", 
+    order : 6, 
+    examples: "Bogosort", 
+    wikiLink :"http://en.wikipedia.org/wiki/Bogosort"});
 
 Template.body.helpers({
     notations : [
-      {name : "constant", formula : "O(1)", order : 0},
-      {name : "logarithmic", formula : "O(log n)", order : 1},
-      {name : "linear", formula : "O(n)", order : 2},
-      {name : "quadratic", formula : "O(n^2)", order : 3},
-      {name : "expotential", formula : "O(c^n), c > 1", order : 4},
-      {name : "factorial", formula : "O(n!)", order : 5},
-      {name : "n * n factorial", formula : "O(n * n!)", order : 6}
+      {name : "constant"},
+      {name : "logarithmic"},
+      {name : "linear"},
+      {name : "quadratic"},
+      {name : "expotential"},
+      {name : "factorial"},
+      {name : "n * n factorial"}
    ]
 });
 
@@ -52,22 +82,12 @@ Template.body.helpers({
     }
   });
 
-  // Template.notationElement.helpers({
-  //   notations : function(){
-  //     console.log('Template.notationElement.helpers notations');
-  //     return Notations.find().fetch();
-  //   }
-  // });
-
   Template.body.events({
     "change #notationsList" : function(event){
-      //window.alert('I DUNNO SOMETHING SELECTED HERP ' + event.currentTarget.value);
-      //Template.notationView.__helpers.get('updateView')(event.currentTarget.value);
       Session.set("name",event.currentTarget.value);
     }
   });
 
-//});
 }
 
 if (Meteor.isServer) {
